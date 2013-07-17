c5dfd6181c9872ea9c0efb288ea1eb98facce06d
H.B <revolteur@gmail.com>
2013-07-17 07:00:00 +0000

    replaced hello world with buf.toString('utf8')

--- web.js
+++ web.js
@@ -9,7 +9,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send("Hello Wrold 2!");
+  response.send(buf.toString('utf8'));
 });
 
 var port = process.env.PORT || 5000;
