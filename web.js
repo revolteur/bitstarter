68aa6ad47f26b0fb733336262e353e2f21c31584
H.B <revolteur@gmail.com>
2013-07-17 06:50:00 +0000

    rolled back response send to Hello World and kept the new code

--- web.js
+++ web.js
@@ -9,7 +9,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buf.toString);
+  response.send("Hello Wrold 2!!);
 });
 
 var port = process.env.PORT || 5000;
